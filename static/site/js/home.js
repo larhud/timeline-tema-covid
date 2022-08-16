67f50222f0b28006431c13f0739ba790fcbf2b27
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2022-08-16 18:05:28 -0300

    Ajuste do script de paginação dos termos

--- static/site/js/home.js
+++ static/site/js/home.js
@@ -1,98 +1,51 @@
 $(function () {
-  (function (name) {
-    var container = $("#pagination-" + name);
-    container.pagination({
-      dataSource: [
-        {
-          titulo: "COVID",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "covid",
-        },
-        {
-          titulo: "Dilma Roussef",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time2.png",
-          slug: "dilma",
-        },
-        {
-          titulo: "Timeline 1",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "covid",
-        },
-        {
-          titulo: "Timeline 2",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "covid",
-        },
-        {
-          titulo: "COVID",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "covid",
-        },
-        {
-          titulo: "Dilma Roussef",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "dilma",
-        },
-        {
-          titulo: "Timeline 2",
-          texto:
-            "Linha cronológica (timeline) de informações midiáticas sobre a COVID-19.  O objetivo é produzir...",
-          imagem: "time1.png",
-          slug: "dilma",
-        },
-      ],
-      locator: "items",
-      pageSize: 4,
-      showPageNumbers: true,
-      showPrevious: true,
-      showNext: true,
-      nextText: `Próxima <i class="fa-solid fa-arrow-right"></i>`,
-      prevText: `<i class="fa-solid fa-arrow-left"></i> Anterior`,
-      showFirstOnEllipsisShow: true,
-      showLastOnEllipsisShow: true,
-      ajax: {
-        beforeSend: function () {
-          container.prev().html("Loading data from flickr.com ...");
-        },
-      },
-      callback: function (response, pagination) {
-        var dataHtml = "<div class='itens'>";
+    (function (name) {
+        let container = $("#pagination-" + name);
 
-        $.each(response, function (index, item) {
-          dataHtml +=
-            "<div class='item'><img src='/media/uploads/" +
-            item.imagem +
-            "' /><div class='conteudo'><h4>" +
-            item.titulo +
-            "</h4>";
-          dataHtml += "<p>" + item.texto + "</p>";
-          dataHtml += `<a href=/timeline/` + item.slug + `>Acessar timeline <i class="fa-solid fa-arrow-right"></i></a></div></div>`;
+        container.pagination({
+            locator: "items",
+            pageSize: 4,
+            dataSource: window.__lista_de_termos__,
+            totalNumberLocator: function (data) {
+                // data = retorno da url indicada em dataSource
+                return data.total;
+            },
+            showPageNumbers: true,
+            showPrevious: true,
+            showNext: true,
+            nextText: 'Próxima <i class="fa-solid fa-arrow-right"></i>',
+            prevText: '<i class="fa-solid fa-arrow-left"></i> Anterior',
+            showFirstOnEllipsisShow: true,
+            showLastOnEllipsisShow: true,
+            ajax: {
+                beforeSend: function () {
+                    container.prev().html("Loading data...");
+                },
+            },
+            callback: function (response, pagination) {
+                let dataHtml = '<div class="itens">';
+
+                $.each(response, function (index, item) {
+                    dataHtml +=
+                        '<div class="item"><img src="' + item.imagem + '" alt="' + item.termo +
+                        '"/><div class="conteudo"><h4>' + item.termo + '</h4>';
+                    dataHtml += '<p>' + item.texto + '</p>';
+                    dataHtml += '<a href="' + item.url + '">Acessar timeline <i class="fa-solid fa-arrow-right"></i></a></div></div>';
+                });
+
+                dataHtml += "</div>";
+
+                container.prev().html(dataHtml);
+            },
         });
-
-        dataHtml += "</div>";
-
-        container.prev().html(dataHtml);
-      },
-    });
-  })("demo2");
+    })("termos");
 });
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    paginationClickable: true
-  },
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        paginationClickable: true
+    },
 });
